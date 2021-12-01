fe011b058e3e4ce95536b243cf42d18099e3533b
justalk <justal.kevin@gmail.com>
2021-12-01 05:47:46 +0100

    [FEATURE] Adding effect around image

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -2,11 +2,15 @@ import React, { useRef } from 'react'
 import { useLoader, useFrame } from '@react-three/fiber'
 import { IMAGE } from '@src/constants/layers'
 import * as THREE from 'three'
+import TM from 'gsap'
 import './Materials'
 
 export default function Image() {
   const ref = useRef()
-  const [uTexture] = useLoader(THREE.TextureLoader, ['./images/love_1.jpg'])
+  const [uTexture, uTexture2] = useLoader(THREE.TextureLoader, [
+    './images/love_1.jpg',
+    './images/love_2.jpg'
+  ])
 
   useFrame((state, delta) => {
     ref.current.uTime += delta
@@ -18,10 +22,23 @@ export default function Image() {
       renderOrder={IMAGE}
       onPointerEnter={() => {
         console.log('On It !')
+        TM.to(ref.current.uniforms.uVelo, 1.0, {
+          value: 1,
+          ease: 'expo.out'
+        })
+      }}
+      onPointerLeave={() => {
+        TM.to(ref.current.uniforms.uVelo, 1.0, {
+          value: 0,
+          ease: 'expo.out'
+        })
+      }}
+      onPointerMove={(e) => {
+        ref.current.uMouse = e.intersections[0].uv
       }}
     >
-      <planeGeometry args={[4, 4]} />
-      <imageMaterial ref={ref} uTexture={uTexture} />
+      <planeGeometry args={[4, 4, 8, 8]} />
+      <imageMaterial ref={ref} uTexture={uTexture} uTexture2={uTexture2} />
     </mesh>
   )
 }
--- src/constants/layers.js
+++ src/constants/layers.js
@@ -1,4 +1,4 @@
 export const TRANSITION = 10000
+export const IMAGE = 2000
 export const CURSOR = 1000
-export const IMAGE = 100
 export const SLIDE = 10
