18762abaf34d400962afe46b1e810c89e128e35b
justalk <justal.kevin@gmail.com>
2021-12-01 05:53:51 +0100

    [FEATURE] Adding effect around image

--- src/components/Image/Materials/index.js
+++ src/components/Image/Materials/index.js
@@ -78,9 +78,9 @@ export default class ImageMaterial extends THREE.ShaderMaterial {
       void main()  {
           vec2 newUV = vUv;
           float c = circle(vUv, uMouse, 0.1 + (1.0 - uVelo), 0.05);
-          float r = texture2D(uTexture2, newUV.xy).x;
-          float g = texture2D(uTexture2, newUV.xy).y;
-          float b = texture2D(uTexture2, newUV.xy).z;
+          float r = texture2D(uTexture2, newUV.xy -= c * (0.1 * .15 * uVelo)).x;
+          float g = texture2D(uTexture2, newUV.xy -= c * (0.1 * .15 * uVelo)).y;
+          float b = texture2D(uTexture2, newUV.xy -= c * (0.1 * .15 * uVelo)).z;
           vec4 color = vec4(r, g, b, 1.);
 
           float finalMask = smoothstep(0.4, 0.5, c);
